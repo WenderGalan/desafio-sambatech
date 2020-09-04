912731073d469579969f0456a0776612c025ff2e
Wender Galan <wendergalan2014@hotmail.com>
2020-09-04 18:25:02 -0400

    Inicializando o projeto.

---
+++ src/main/java/wendergalan/github/io/desafiosambatech/DesafioSambatechApplication.java
@@ -0,0 +1,13 @@
+package wendergalan.github.io.desafiosambatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSambatechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSambatechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wendergalan/github/io/desafiosambatech/DesafioSambatechApplicationTests.java
@@ -0,0 +1,13 @@
+package wendergalan.github.io.desafiosambatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSambatechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
