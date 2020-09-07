875c38b38e84dcdfe1d7bf07c292dba1e713d2be
Wender Galan <wendergalan2014@hotmail.com>
2020-09-07 17:55:01 -0400

    - Testando a segurança da API.

--- src/test/java/wendergalan/github/io/desafiosambatech/api/resource/HomeControllerTest.java
+++ src/test/java/wendergalan/github/io/desafiosambatech/api/resource/HomeControllerTest.java
@@ -20,7 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HomeController.class)
 @AutoConfigureMockMvc
-@WithMockUser(username = "admin", password = "admin", roles = "ADMIN")
 public class HomeControllerTest {
 
     @Autowired
@@ -31,6 +30,7 @@ public class HomeControllerTest {
 
     @Test
     @DisplayName("Deve redirecionar para a documentação do Swagger.")
+    @WithMockUser(username = "admin", password = "admin", roles = "ADMIN")
     public void redirectSwaggerTest() throws Exception {
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .get("/")
@@ -39,5 +39,17 @@ public class HomeControllerTest {
         mvc
                 .perform(request)
                 .andExpect(status().isFound());
+    }
+
+    @Test
+    @DisplayName("Deve dar não autorizado ao tentar acessar a API sem credenciais.")
+    public void redirectSwaggerUnauthorizedTest() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc
+                .perform(request)
+                .andExpect(status().isUnauthorized());
     }
 }
